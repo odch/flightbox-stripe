cb90891c0aa710e97355baecc6e06c7015db1e4b
Roland Züger <roland.zueger@gmail.com>
2025-11-25 10:07:18 +0100

    Upgrade Firebase SDK to v4.6.0
    From v4.6.0 on, Firebase RTDB URLs for databases in Europe can be
    specified. Before this version, only *.firebaseio.com URLs are allowed

--- test/status.go
+++ test/status.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"log"
 
-	firebase "firebase.google.com/go"
+	firebase "firebase.google.com/go/v4"
 )
 
 func UpdateStatus(config *Config, ctx context.Context, id string, status string) error {
